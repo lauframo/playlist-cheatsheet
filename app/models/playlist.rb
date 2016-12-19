5fd0ac9ef87c82074cb1b9b0a7f3be2434bc2a08
Laura Moreno <stereofonik.imgs@gmail.com>
2016-12-19 14:35:47 -0600

    define spotify playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -3,4 +3,8 @@ class Playlist < ApplicationRecord
 	validates_uniqueness_of :playlist_id
 	validates_presence_of :username, :playlist_id
 
+	def spotify_playlist
+		@spotify_playlist = RSpotify::Playlist.find(self.username, self.playlist_id)
+	end
+
 end

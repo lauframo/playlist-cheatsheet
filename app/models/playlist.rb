6e51e0e1c09e7dd819aa5403d4e140b3b5b04fc4
Laura Moreno <stereofonik.imgs@gmail.com>
2016-12-22 20:45:12 -0500

    define and test get_summary private method

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,10 +1,10 @@
 class Playlist < ApplicationRecord
 
-
-	validates uniqueness: :playlist_id
-	validates_presence_of :username, :playlist_id
-
 	has_many :songs, through: :plalylist_songs
+
+	validates_presence_of :username, :playlist_id
+	validates :playlist_id, uniqueness: true
+
 
 	def spotify_playlist
 		@spotify_playlist = RSpotify::Playlist.find(self.username, self.playlist_id)
@@ -14,8 +14,16 @@ class Playlist < ApplicationRecord
 		tracks = self.spotify_playlist.tracks
 		tracks.each do |track|
 			artist = Artist.find_or_create_by(name: track.artists.first.name)
-			album = Album.find_or_create_by(name: track.album.name, artist_id: artist)
-			Song.find_or_create_by(name: track.name, artist_id: artist, album_id: album)
+			p artist
+			p "-------"
+			album = Album.find_or_create_by(name: track.album.name, artist_id: artist.id)
+			p album
+			p "--------"
+			p track.album.name
+			p album.name
+			p album.summary
+			p album.errors.full_messages
+			# Song.find_or_create_by(name: track.name, artist_id: artist, album_id: album)
 		end
 	end
 
